1f509d61a5a66a8e3444a7717a799f0600532356
Gustavo Lucena <gustavolucena12@gmail.com>
2022-05-14 19:38:34 -0300

    Pagination

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -1,9 +1,8 @@
-
 import {ReactComponent as StarFull} from "assets/img/star-full.svg";
 import {ReactComponent as StarHalf} from "assets/img/star-half.svg";
 import {ReactComponent as StarEmpty} from "assets/img/star-empty.svg";
 import "./styles.css"
-import { type } from "@testing-library/user-event/dist/type";
+
 
 type Props = {
   score: number;
